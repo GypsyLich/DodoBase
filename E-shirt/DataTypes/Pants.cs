0a29e135c7d357f26530f0e4cfec2a4cccc6516d
Another man <goowillprevail@gmail.com>
2018-11-11 14:10:53 +0300

    get rid of usings

--- E-shirt/DataTypes/Pants.cs
+++ E-shirt/DataTypes/Pants.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace E_shirt.DataTypes
+﻿namespace E_shirt.DataTypes
 {
     class Pants : Clothes
     {
